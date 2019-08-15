862e8e99b8a0f59f28ab59c78a7f5678f8c90b70
Jose Veliz <jveliz3821@gmail.com>
2019-08-14 21:02:52 -0400

    Add edit functionality and clean up with partials

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: [:show, :edit, :update]
 
   def new
     @student = Student.new
@@ -15,11 +16,26 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @student = set_student
+    # @student = set_student #setting student above
   end
 
   def index
     @students = Student.all
+  end
+
+  def edit
+    # @student = set_student #setting student above
+  end
+
+  def update
+    # @student = set_student #setting student above
+
+    if @student.update(student_params)
+      flash[:success] = "User account updated"
+      redirect_to student_path(@student)
+    else
+      render 'edit'
+    end
   end
 
   private
